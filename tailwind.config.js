20c2adc9e8ed12b7a65592f0abfbf376eb0035b6
Riandra4trend <18221077@std.stei.itb.ac.id>
2023-12-14 14:01:17 +0700

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+
+    content: {
+  
+      relative: true,
+  
+      files: ["./app/Views/*.php", "./public/*.js"],
+  
+    },
+  
+    theme: {
+  
+    extend: {},
+  
+  },
+  
+  plugins: [],
+  
+  };

882bc2fdd014047df22195601db76c29a196e2fc
Muhammad Hanif Al Faithoni <et4w4zz@gmail.com>
2023-12-19 04:52:19 +0700

    fix: create transaksi

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,7 @@ $routes->get('/', 'Pages::index');
 $routes->post('/login_action', 'LoginController::login_action');
 $routes->get('pages/login', 'Pages::login');
 $routes->get('pages/dashboard', 'Pages::dashboard');
-$routes->post('/purchase', 'Pages::purchase1');
+$routes->post('/purchase', 'Pages::purchase');
 $routes->get('pages/restock', 'Pages::restock');
 $routes->get('pages/edit/(:num)', 'Pages::edit/$1');
 $routes->post('pages/update/(:num)', 'Pages::update/$1');
